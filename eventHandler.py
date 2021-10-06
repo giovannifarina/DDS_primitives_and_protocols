e281a86dffa4c50b962a638eef86fd43a86cd10e
giovannifarina <gfarina@diag.uniroma1.it>
2021-10-06 14:45:00 +0200

    Update eventHandler.py

--- eventHandler.py
+++ eventHandler.py
@@ -2,10 +2,10 @@ from DDSlogger import logger
 import sys
 
 def handleEvents(eventQueue, handlerFunction):
-    try:
-        while True:
+    while True:
+        try:        
             e = eventQueue.get()
             handlerFunction(*e)
-    except Exception as ex:
-        _, _, exc_tb = sys.exc_info()
-        logger.debug('Exception in '+str(sys._getframe(  ).f_code.co_name)+":"+str(exc_tb.tb_lineno)+" - "+str(type(ex))+' : '+str(ex))
+        except Exception as ex:
+            _, _, exc_tb = sys.exc_info()
+            logger.debug('Exception in '+str(sys._getframe(  ).f_code.co_name)+":"+str(exc_tb.tb_lineno)+" - "+str(type(ex))+' : '+str(ex))
