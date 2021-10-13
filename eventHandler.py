a93298d6e757ead21a998ce96a9fb997e4496bdf
giovannifarina <gfarina@diag.uniroma1.it>
2021-10-13 12:09:07 +0200

    Update eventHandler.py

--- eventHandler.py
+++ eventHandler.py
@@ -8,4 +8,4 @@ def handleEvents(eventQueue, handlerFunction):
             handlerFunction(*e)
         except Exception as ex:
             _, _, exc_tb = sys.exc_info()
-            logger.debug('Exception in '+str(sys._getframe(  ).f_code.co_name)+":"+str(exc_tb.tb_lineno)+" - "+str(type(ex))+' : '+str(ex))
+            logger.debug('Exception in '+str(sys._getframe(  ).f_code.co_name)+":"+str(exc_tb.tb_lineno)+" - "+str(type(ex))+' : '+str(ex)+" - event: "+str(e)+' - function handler: '+str(handlerFunction))
