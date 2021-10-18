cfa6e23ec2d1dfd3ad54230b172e3742a3af63f3
giovannifarina <gfarina@diag.uniroma1.it>
2021-10-18 14:50:28 +0200

    added support to custom topologies
    - custom topologies support
    - some functions/classes renamig

--- DS_simulation.py
+++ DS_simulation.py
@@ -8,6 +8,9 @@ from mininet.cli import CLI
 from mininet.link import TCLink
 #from mininet.node import CPULimitedHost
 
+import networkx as nx
+from ipaddress import IPv4Network
+
 import configparser
 import logging
 
@@ -15,8 +18,8 @@ import logging
 config = configparser.ConfigParser()
 config['LOG'] = {}
 config['LOG']['level'] = str(logging.DEBUG)
-config['LOG']['fairlosslink'] = 'true'
-config['LOG']['stubbornlink'] = 'true'
+config['LOG']['fairlosslink'] = 'false'
+config['LOG']['stubbornlink'] = 'false'
 config['LOG']['perfectlink'] = 'true'
 config['LOG']['perfectfailuredector'] = 'true'
 config['LOG']['name'] = 'DDS'
@@ -36,6 +39,18 @@ class SingleSwitchTopo(Topo):
             host = self.addHost('h%s' % (h + 1))
             self.addLink(host, switch, bw=10, delay='150ms', loss=0)
 
+class ThreeHosts(Topo):
+
+    def build(self):
+        h1 = self.addHost('h1', ip='192.168.0.1/30')
+        h2 = self.addHost('h2', ip='192.168.0.2/30')
+        h3 = self.addHost('h3', ip='192.168.0.6/30')
+
+        self.addLink(h1,h2)
+        self.addLink(h1,h3, intfName1='h1-eth1', params1={ 'ip' : '192.168.0.5/30' })
+        self.addLink(h2,h3, intfName1='h2-eth1', params1={ 'ip' : '192.168.0.9/30' }, intfName2='h3-eth1', params2={ 'ip' : '192.168.0.10/30' })
+
+
 # generate configuration files for mininet simulations
 #§NOTE it currently handle only complete communication networks
 def generateProcessConfigurationFiles(net):
@@ -53,13 +68,34 @@ def simpleTest(n_processes):
     net = Mininet(topo=topo, link=TCLink)
     net.start()
     generateProcessConfigurationFiles(net)
-    for host in net.hosts:
-        host.cmd('python3 process.py &') # run process.py on host
+    #for host in net.hosts:
+    #    host.cmd('python3 process.py &') # run process.py on host
     CLI(net)
     net.stop()
 
+def simpleTest2():
+    topo = ThreeHosts()
+    net = Mininet(topo=topo, link=TCLink)
+    net.start()
+    """
+    generateProcessConfigurationFiles(net)
+    for host in net.hosts:
+        host.cmd('python3 process.py &') # run process.py on host
+    """
+    CLI(net)
+    net.stop()
+
+def testNetworkxTopology():
+    G = nx.cycle_graph(10)
+    for i in range(3):
+        for node in G.nodes:
+            for edge in G.edges(node):
+                print(node,edge)
+        print()
 
 if __name__ == '__main__':
     # Tell mininet to print useful information
     setLogLevel('info')
-    simpleTest(n_processes=5)  
+    #simpleTest(n_processes=5) 
+    #simpleTest2()
+    testNetworkxTopology()
